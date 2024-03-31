966555c10a35ef3268de224a48a0b4f1dbb9d8e3
kornej8 <deluxehaha@gmail.com>
2024-03-31 18:43:32 +0300

    update

--- black_box.py
+++ black_box.py
@@ -8,6 +8,11 @@ import matplotlib.pyplot as plt
 import pytensor
 import pytensor.tensor as pt
 from pycalphad import Database, equilibrium
+from scipy import optimize
+
+import numpy as np
+from scipy import optimize
+
 
 # define a pytensor Op for our likelihood function
 class LogLike(pt.Op):
@@ -45,17 +50,59 @@ class LogLike(pt.Op):
         self.tdb = tdb_object
         self.params = params
         self.func = func
+        self.logpgrad = LogLikeGrad(self.func, self.data, self.tdb)
 
     def perform(self, node, inputs, outputs):
         # the method that is used when calling the Op
         (theta,) = inputs  # this will contain my variables
 
-        self.params = {'L0_FCC' : inputs[0][0]}
-
+        self.params = inputs[0][0]
+        # params, df, tdb_object
         # call the log-likelihood function
-        logl = self.func(self.data, self.tdb_object, self.params)
+        logl = self.func(self.params, self.data, self.tdb)
 
         outputs[0][0] = logl  # output the log-likelihood
 
+    def grad(self, inputs, g):
+        # the method that calculates the gradients - it actually returns the
+        # vector-Jacobian product - g[0] is a vector of parameter values
+        (theta,) = inputs  # our parameters
+        return [g[0] * self.logpgrad(theta)]
 
+class LogLikeGrad(pt.Op):
+
+    """
+    This Op will be called with a vector of values and also return a vector of
+    values - the gradients in each dimension.
+    """
+
+    itypes = [pt.dvector]
+    otypes = [pt.dvector]
+
+    def __init__(self, func, data, tdb):
+        """
+        Initialise with various things that the function requires. Below
+        are the things that are needed in this particular example.
+
+        Parameters
+        ----------
+        data:
+            The "observed" data that our log-likelihood function takes in
+        x:
+            The dependent variable (aka 'x') that our model requires
+        sigma:
+            The noise standard deviation that out function requires.
+        """
+
+        # add inputs as class attributes
+        self.func = func
+        self.data = data
+        self.tdb = tdb
+    def perform(self, node, inputs, outputs):
+        (theta,) = inputs
+        # calculate gradients
+        # params, df, tdb_object
+        grads = optimize.approx_fprime(theta, self.func, np.sqrt(np.finfo(float).eps), self.data, self.tdb)
+
+        outputs[0][0] = grads
 
--- get_exp.py
+++ get_exp.py
@@ -15,8 +15,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pytensor
 import pytensor.tensor as pt
+from pymc.variational.callbacks import CheckParametersConvergence
 
-print('start')
+
 ELEM = 'CR'
 tdb = 'CoCr-01Oik.tdb'
 tdb_object = f"./test_data/{tdb}"
@@ -34,36 +35,72 @@ PSS Пока работает только с 'CoCr-01Oik.tdb' потому чт
 
 """
 
-def get_x_based_on_experement(df, tdb_object, params = None):
-    print('123')
-    tdb_object.model_params = params
+
+
+def get_x_based_on_experement(params, df, tdb_object):
+    return_square_of_diff = lambda first_var, second_var: (first_var - second_var) ** 2
+
+    if params is None:
+        tdb_object.model_params = None
+    else:
+        tdb_object.model_params = {'L0_FCC': params}
+
     df[f'conc_from_{tdb}'] = df['T'].astype(str) + ';' + df['phase']
     df[f'conc_from_{tdb}'] = df[f'conc_from_{tdb}'].apply(lambda x: tdb_object.get_params(
         t=float(x.split(';')[0]),
         checked_phase=x.split(';')[1]).get_max_concentration()[0])
-    return np.array(df['conc_from_CoCr-01Oik.tdb'].astype(np.float32).values)
+    df['diffs_values'] = df.apply(lambda table: return_square_of_diff(table.cr_conc, table[f'conc_from_{tdb}']), axis=1)
+    return np.array(df['diffs_values'].sum())
 
 
 def main():
+    pytensor.config.exception_verbosity = 'high'
     tdb_object = TDBPoints(tdb_object_path, element=ELEM)
     df = pd.read_excel(path)
-    print(df)
     logl = LogLike(get_x_based_on_experement, df, tdb_object)
+    observed_data = df.cr_conc
 
     with pm.Model() as model:
-        L0_FCC = pm.Normal("L0_FCC", mu=-12000, sigma=1)
+        vL0_FCC = pm.Normal("vL0_FCC", mu=-12000, sigma=1)
 
-        theta = pt.as_tensor_variable([L0_FCC])
+        theta = pt.as_tensor_variable([vL0_FCC])
 
-        # use a Potential to "call" the Op and include it in the logp computation
-        pm.Potential("likelihood", logl(theta))
+        cr_observed = pm.ConstantData(name='cr_observed', value=observed_data)
+        log = pm.Normal('y', logl(theta),  sigma=1, observed=cr_observed)
 
+    # with model:
+    #     print('tut')
+    #     mean_field = pm.fit(method = 'advi', obj_optimizer=pm.adagrad_window(learning_rate=1e-2))
 
-    with model:
-        mean_field = pm.fit(method="advi")
+        # advi = pm.ADVI()
 
-    az.plot_posterior(mean_field.sample(1000), color="LightSeaGreen")
-    plt.show()
+        # tracker = pm.callbacks.Tracker(
+        #     mean=advi.approx.mean.eval,  # callable that returns mean
+        #     std=advi.approx.std.eval,  # callable that returns std
+        # )
+        #
+        # approx = advi.fit(20000, callbacks=[tracker])
+        #
+        # fig = plt.figure(figsize=(16, 9))
+        # mu_ax = fig.add_subplot(221)
+        # std_ax = fig.add_subplot(222)
+        # hist_ax = fig.add_subplot(212)
+        # mu_ax.plot(tracker["mean"])
+        # mu_ax.set_title("Mean track")
+        # std_ax.plot(tracker["std"])
+        # std_ax.set_title("Std track")
+        # hist_ax.plot(advi.hist)
+        # hist_ax.set_title("Negative ELBO track");
 
+        # Use custom number of draws to replace the HMC based defaults
+        # # use a Potential to "call" the Op and include it in the logp computation
+        # pm.Potential("likelihood", logl(theta))
+        #
+        # likelihood = pm.Normal('y', mu=logl(theta), sigma=0.001, observed=cr_observed)
+        #
+        with model:
+            mean_field = pm.fit(method="advi", callbacks=[CheckParametersConvergence()])
+    #
+    plt.plot(mean_field.hist);
 if __name__ == '__main__':
     main()
